008345bd3b7423d262c293d201dac807e2a756c2
sajibsaha4647 <sksaha4648@gmail.com>
2021-10-29 16:31:00 +0600

    first commit

---
+++ app/src/main/java/com/example/aswitch/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.aswitch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Switch aSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        aSwitch = findViewById(R.id.switchbtn);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    Toast.makeText(MainActivity.this,"on",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"off",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+    }
+}
